ffb3eebca33c267160518724e3b260bb96934141
animaxxpk <animaxxpk@gmail.com>
2026-07-01 18:27:50 +0500

    playlist.js

--- api/playlist.js
+++ api/playlist.js
@@ -1,15 +1,21 @@
 export default async function handler(req, res) {
   const { user, pass } = req.query;
 
-  if (user !== "ahmer" || pass !== "8800") {
+  // 🔐 USER CHECK
+  const u = await fetch("https://raw.githubusercontent.com/animaxxpk/m3u-AniVerseX/main/users.json");
+  const users = await u.json();
+
+  const valid = users.find(
+    x => x.user === user && x.pass === pass && x.active === true
+  );
+
+  if (!valid) {
     return res.status(401).send("#EXTM3U\n# Unauthorized");
   }
 
-  const data = await fetch(
-    "https://raw.githubusercontent.com/animaxxpk/m3u-AniVerseX/main/channels.json"
-  );
-
-  const channels = await data.json();
+  // 📺 CHANNEL LIST
+  const c = await fetch("https://raw.githubusercontent.com/animaxxpk/m3u-AniVerseX/main/channels.json");
+  const channels = await c.json();
 
   let m3u = "#EXTM3U\n";
 
