6e970aca1717dabdaae0c2d96de1595f7b414bdf
animaxxpk <animaxxpk@gmail.com>
2026-07-01 18:21:37 +0500

    playlist.js

--- api/playlist.js
+++ api/playlist.js
@@ -1,15 +1,23 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { user, pass } = req.query;
 
-  // CHECK LOGIN
   if (user !== "ahmer" || pass !== "8800") {
-    res.setHeader("Content-Type", "text/plain");
     return res.status(401).send("#EXTM3U\n# Unauthorized");
   }
 
-  res.setHeader("Content-Type", "text/plain");
+  const data = await fetch(
+    "https://raw.githubusercontent.com/animaxxpk/m3u-AniVerseX/main/channels.json"
+  );
 
-  return res.send(`#EXTM3U
-#EXTINF:-1,Test Channel
-https://test-streams.mux.dev/test_001/stream.m3u8`);
+  const channels = await data.json();
+
+  let m3u = "#EXTM3U\n";
+
+  channels.forEach(ch => {
+    m3u += `#EXTINF:-1,${ch.name}\n`;
+    m3u += `https://m3u-system.vercel.app/api/stream?id=${ch.id}&user=${user}&pass=${pass}\n`;
+  });
+
+  res.setHeader("Content-Type", "text/plain");
+  res.send(m3u);
 }
