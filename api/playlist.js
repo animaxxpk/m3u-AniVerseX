d91bfa94187ab6d7abc8b389c5cabf5787c61d05
animaxxpk <animaxxpk@gmail.com>
2026-07-01 18:08:01 +0500

    playlist.js

--- api/playlist.js
+++ api/playlist.js
@@ -1,18 +1,29 @@
-export default function handler(req, res) {
-  try {
-    const { user, pass } = req.query;
+export default async function handler(req, res) {
+  const { user, pass } = req.query;
 
-    if (user !== "ahmer" || pass !== "7861") {
-      return res.status(401).send("#EXTM3U\n# Unauthorized");
-    }
+  // GET USERS
+  const u = await fetch("https://raw.githubusercontent.com/animaxxpk/m3u-AniVerseX/main/users.json");
+  const users = await u.json();
 
-    res.setHeader("Content-Type", "text/plain");
+  const validUser = users.find(
+    x => x.user === user && x.pass === pass && x.active === true
+  );
 
-    return res.send(`#EXTM3U
-#EXTINF:-1,Test Channel
-https://test-streams.mux.dev/test_001/stream.m3u8`);
-
-  } catch (e) {
-    return res.status(500).send("#EXTM3U\n# Server Error");
+  if (!validUser) {
+    return res.status(401).send("#EXTM3U\n# Unauthorized");
   }
+
+  // GET CHANNELS
+  const c = await fetch("https://raw.githubusercontent.com/animaxxpk/m3u-AniVerseX/main/channels.json");
+  const channels = await c.json();
+
+  let m3u = "#EXTM3U\n";
+
+  channels.forEach(ch => {
+    m3u += `#EXTINF:-1,${ch.name}\n`;
+    m3u += `https://m3u-system.vercel.app/api/stream?id=${ch.id}&user=${user}&pass=${pass}\n`;
+  });
+
+  res.setHeader("Content-Type", "text/plain");
+  res.send(m3u);
 }
