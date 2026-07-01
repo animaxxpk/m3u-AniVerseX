cf703a561f09cff63c565a822cf04994cee5c174
animaxxpk <animaxxpk@gmail.com>
2026-07-01 18:47:36 +0500

    playlist.js

--- api/playlist.js
+++ api/playlist.js
@@ -2,7 +2,9 @@ export default async function handler(req, res) {
   try {
     const { user, pass } = req.query;
 
-    if (user !== "ahmer" || pass !== "8800") {
+   if (
+(user !== "ahmer" || pass !== "8800") &&
+(user !== "ali" || pass !== "1234") &&
       res.setHeader("Content-Type", "text/plain");
       return res.status(401).send("#EXTM3U\n# Unauthorized");
     }
