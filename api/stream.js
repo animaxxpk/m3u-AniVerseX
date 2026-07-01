e34255c1d055080b4df0f0dec1be69540b8a3e09
animaxxpk <animaxxpk@gmail.com>
2026-07-01 19:32:01 +0500

    stream.js

--- api/stream.js
+++ api/stream.js
@@ -1,23 +1,12 @@
 export default async function handler(req, res) {
   const { id, user, pass } = req.query;
 
-  console.log("REQ:", req.query);
-
-  const users = {
-    ahmer: "8800",
-    ali: "1234"
-  };
-
-  if (!(user in users)) {
-    return res.status(401).send("Unauthorized user");
-  }
-
-  if (users[user] !== pass) {
-    return res.status(401).send("Wrong password");
+  if (user !== "ahmer" || pass !== "8800") {
+    return res.status(401).send("Unauthorized");
   }
 
   const map = {
-    "1": "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/1.ts"
+    "1": "https://test-streams.mux.dev/test_001/stream.m3u8"
   };
 
   const url = map[id];
@@ -26,5 +15,6 @@ export default async function handler(req, res) {
     return res.status(404).send("Not found");
   }
 
+  // 🔥 IMPORTANT FIX
   return res.redirect(302, url);
 }
