e00d98301fe72e977ffd628ac1c04a32593b7169
animaxxpk <animaxxpk@gmail.com>
2026-07-02 15:44:09 +0500

    users.js

---
+++ api/users.js
@@ -0,0 +1,16 @@
+export const users = {
+  ahmer: {
+    password: "8888",
+    active: true
+  },
+
+  ali: {
+    password: "1234",
+    active: true
+  },
+
+  test: {
+    password: "0000",
+    active: false
+  }
+};
