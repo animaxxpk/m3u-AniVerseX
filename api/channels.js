c1babb31c033127d41cf50c4acc4bc6a5987cc2d
animaxxpk <animaxxpk@gmail.com>
2026-07-02 15:57:31 +0500

    channels.js

--- api/channels.js
+++ api/channels.js
@@ -1,7 +1,14 @@
 export const channels = [
   {
     id: "1",
-    name: "Test Channel",
-    url: "https://test-streams.mux.dev/test_001/stream.m3u8"
+    name: "Saudi Quran",
+    category: "PK | ISLAMIC",
+    url: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/1.ts"
+  },
+   {
+    id: "2",
+    name: "Saudi Al Sunnah Al Naavi",
+    category: "PK | ISLAMIC",
+    url: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/2.ts"
   }
-];
+  ];
