2fecaf2ade3b77d22c36099d3fccf794e88cac45
animaxxpk <animaxxpk@gmail.com>
2026-07-02 15:48:29 +0500

    stream.js

--- api/stream.js
+++ api/stream.js
@@ -1,20 +1,24 @@
+import { users } from "./users";
+import { channels } from "./channels";
+
 export default async function handler(req, res) {
-  const { id, user, pass } = req.query;
+  try {
+    const { id, user, pass } = req.query;
 
-  if (user !== "ahmer" || pass !== "8888") {
-    return res.status(401).send("Unauthorized");
+    if (!users[user] || users[user] !== pass) {
+      return res.status(401).send("Unauthorized");
+    }
+
+    const channel = channels.find((c) => c.id === id);
+
+    if (!channel) {
+      return res.status(404).send("Not Found");
+    }
+
+    return res.redirect(302, channel.url);
+
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Server Error");
   }
-
-  const map = {
-    "1": "https://test-streams.mux.dev/test_001/stream.m3u8"
-  };
-
-  const url = map[id];
-
-  if (!url) {
-    return res.status(404).send("Not found");
-  }
-
-  // 🔥 IMPORTANT FIX
-  return res.redirect(302, url);
 }
