257c027a77f2aa01c7a62818f9ce824dbd0c426e
animaxxpk <animaxxpk@gmail.com>
2026-07-04 01:06:11 +0500

    playlist.js

--- api/playlist.js
+++ api/playlist.js
@@ -1,36 +1,27 @@
 import { users } from "./users";
 import { channels } from "./channels";
 
-export default async function handler(req, res) {
-  try {
-    const { user, pass } = req.query;
+export default function handler(req, res) {
+  const { user, pass } = req.query;
 
-    const account = users[user];
+  const account = users[user];
 
-    // Username / Password check
-    if (!account || account.password !== pass) {
-      res.setHeader("Content-Type", "text/plain");
-      return res.status(401).send("#EXTM3U\n# Unauthorized");
-    }
-
-    // Expiry check
-    if (account.expiry && new Date() > new Date(account.expiry)) {
-      res.setHeader("Content-Type", "text/plain");
-      return res.status(403).send("#EXTM3U\n# Account Expired");
-    }
-
-    let m3u = "#EXTM3U\n";
-
-    channels.forEach((ch) => {
-      m3u += `#EXTINF:-1 tvg-id="${ch.name}" group-title="${ch.category}",${ch.name}\n`;
-      m3u += `${req.headers["x-forwarded-proto"] || "https"}://${req.headers.host}/api/stream?id=${ch.id}&user=${user}&pass=${pass}\n`;
-    });
-
-    res.setHeader("Content-Type", "text/plain");
-    return res.status(200).send(m3u);
-
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send("#EXTM3U\n# Server Error");
+  if (!account || account.password !== pass) {
+    return res.status(401).send("Invalid username or password");
   }
+
+  const lines = ["#EXTM3U"];
+
+  channels.forEach(ch => {
+    lines.push(
+      `#EXTINF:-1 tvg-id="${ch.id}" tvg-name="${ch.name}" tvg-logo="${ch.logo || ""}" group-title="${ch.category}",${ch.name}`
+    );
+
+    lines.push(
+      `https://nex-tv.vercel.app/api/stream?id=${ch.id}&user=${user}&pass=${pass}`
+    );
+  });
+
+  res.setHeader("Content-Type", "application/x-mpegURL");
+  res.send(lines.join("\n"));
 }
