71b816aad9aaadfa94faf60767a85c2342a545ed
animaxxpk <animaxxpk@gmail.com>
2026-07-04 01:05:08 +0500

    get.js

--- api/get.js
+++ api/get.js
@@ -1,20 +1,10 @@
-export default async function handler(req, res) {
+import playlist from "./playlist";
+
+export default function handler(req, res) {
   const { username, password } = req.query;
 
-  if (username !== "ahmer" || password !== "8888") {
-    return res.status(401).send("Invalid username or password");
-  }
+  req.query.user = username;
+  req.query.pass = password;
 
-  const playlist = `#EXTM3U
-
-#EXTINF:-1 tvg-id="1" tvg-name="Saudi Quran" group-title="Islamic",Saudi Quran
-https://nex-tv.vercel.app/api/stream?id=1&user=${username}&pass=${password}
-
-#EXTINF:-1 tvg-id="2" tvg-name="PTV Sports" group-title="Sports",PTV Sports
-https://nex-tv.vercel.app/api/stream?id=2&user=${username}&pass=${password}
-`;
-
-  res.setHeader("Content-Type", "application/x-mpegURL");
-  res.send(playlist);
+  return playlist(req, res);
 }
-stream_url: `https://nex-tv.vercel.app/api/stream?id=${ch.id}`,
