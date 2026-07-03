1fe0790f4a7d0f58ee3b58e2402cad08cb380f52
animaxxpk <animaxxpk@gmail.com>
2026-07-04 00:44:14 +0500

    channels.js

--- api/channels.js
+++ api/channels.js
@@ -2,13 +2,24 @@ export const channels = [
   {
     id: "1",
     name: "Saudi Quran",
-    category: "PK | ISLAMIC",
-    url: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/1.ts"
+    category: "ISLAMIC",
+    logo: "",
+    stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/2.ts"
   },
-   {
+
+  {
     id: "2",
-    name: "Saudi Al Sunnah Al Naavi",
-    category: "PK | ISLAMIC",
-    url: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/2.ts"
+    name: "PTV Sports",
+    category: "SPORTS",
+    logo: "",
+    stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/2.ts"
+  },
+
+  {
+    id: "3",
+    name: "Geo News",
+    category: "NEWS",
+    logo: "",
+    stream: "http://YOUR_SERVER/live/user/pass/3.ts"
   }
-  ];
+];
