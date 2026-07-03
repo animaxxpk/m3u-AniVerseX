0a9f270279cd4faafb6a82bba5a4d33fc3356482
animaxxpk <animaxxpk@gmail.com>
2026-07-04 01:37:17 +0500

    channels.js

--- api/channels.js
+++ api/channels.js
@@ -1,25 +1,317 @@
 export const channels = [
+{
+  id: "1",
+  name: "Saudi Quran",
+  category: "PK | ISLAMIC",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/1.ts"
+},
+{
+  id: "2",
+  name: "Saudi Al Sunnah Al Naavi",
+  category: "PK | ISLAMIC",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/2.ts"
+},
+{
+  id: "3",
+  name: "Quran Tv",
+  category: "PK | ISLAMIC",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/33756.ts"
+},
+{
+  id: "4",
+  name: "Paigham TV",
+  category: "PK | ISLAMIC",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/3017.ts"
+},
+{
+  id: "5",
+  name: "Al Ehsan",
+  category: "PK | ISLAMIC",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/225460.ts"
+},
+{
+  id: "6",
+  name: "Ary Qtv",
+  category: "PK | ISLAMIC",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/7.ts"
+},
+{
+  id: "7",
+  name: "Madani TV",
+  category: "PK | ISLAMIC",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/3.ts"
+},
+{
+  id: "8",
+  name: "QURAN MAJEED 24/7",
+  category: "PK | ISLAMIC",
+  logo: "",
+  stream: "http://rolextv.one/live/SyedSobanHaidar/03215838388/144707.m3u8"
+},
+{
+  id: "9",
+  name: "NAATS 24/7",
+  category: "PK | ISLAMIC",
+  logo: "",
+  stream: "http://rolextv.one/live/SyedSobanHaidar/03215838388/59974.m3u8"
+},
+{
+  id: "10",
+  name: "Peace TV English HD",
+  category: "PK | ISLAMIC",
+  logo: "",
+  stream: "http://rolextv.one/live/SyedSobanHaidar/03215838388/9.m3u8"
+},
+{
+  id: "11",
+  name: "Paigham Pashto TV",
+  category: "PK | ISLAMIC",
+  logo: "",
+  stream: "http://rolextv.one/live/SyedSobanHaidar/03215838388/225461.m3u8"
+},
+{
+  id: "12",
+  name: "Ary News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/51.ts"
+},
+{
+  id: "13",
+  name: "Geo News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/55.ts"
+},
+{
+  id: "14",
+  name: "Geo Tez",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/56.ts"
+},
+{
+  id: "15",
+  name: "Express News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/54.ts"
+},
   {
-    id: "1",
-    name: "Cartoon Network",
-    category: "PK | KIDS CARTOON",
-    logo: "",
-    stream: "https://s3.ideationtec.live/Cartoon_Network/Cartoon_Network.m3u8"
-  },
-
+  id: "16",
+  name: "GTV News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/58.ts"
+},
+{
+  id: "17",
+  name: "GNN News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/57.ts"
+},
+{
+  id: "18",
+  name: "AJJ News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/48.ts"
+},
+{
+  id: "19",
+  name: "HUM News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/59.ts"
+},
+{
+  id: "20",
+  name: "ABN News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/50.ts"
+},
+{
+  id: "21",
+  name: "365 News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/62185.ts"
+},
+{
+  id: "22",
+  name: "AIK News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/211.ts"
+},
+{
+  id: "23",
+  name: "ABBTAKK News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/49.ts"
+},
+{
+  id: "24",
+  name: "BOL News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/52.ts"
+},
+{
+  id: "25",
+  name: "DUNYA News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/53.ts"
+},
+{
+  id: "26",
+  name: "NEO News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/62.ts"
+},
+{
+  id: "27",
+  name: "PUBLIC News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/80.ts"
+},
+{
+  id: "28",
+  name: "7 NEWS",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one/live/SyedSobanHaidar/03215838388/367625.m3u8"
+},
+{
+  id: "29",
+  name: "92 News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/70.ts"
+},
+{
+  id: "30",
+  name: "SNN NEWS",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one/live/SyedSobanHaidar/03215838388/367622.m3u8"
+},
   {
-    id: "2",
-    name: "PTV Sports",
-    category: "SPORTS",
-    logo: "",
-    stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/2.ts"
-  },
-
-  {
-    id: "3",
-    name: "Geo News",
-    category: "NEWS",
-    logo: "",
-    stream: "http://YOUR_SERVER/live/user/pass/3.ts"
-  }
-];
+  id: "31",
+  name: "24 News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/69.ts"
+},
+{
+  id: "32",
+  name: "METRO 1 News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/61.ts"
+},
+{
+  id: "33",
+  name: "News ONE",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/63.ts"
+},
+{
+  id: "34",
+  name: "DAWN News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/76.ts"
+},
+{
+  id: "35",
+  name: "Digital Pakistan Dispatch",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "https://cdn22lhr.tamashaweb.com:8087/jazzauth/Digital-pak-abr/playlist.m3u8"
+},
+{
+  id: "36",
+  name: "SUNO News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/79.ts"
+},
+{
+  id: "37",
+  name: "BBC News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one/live/SyedSobanHaidar/03215838388/73.m3u8"
+},
+{
+  id: "38",
+  name: "TRT World",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one/live/SyedSobanHaidar/03215838388/25886.m3u8"
+},
+{
+  id: "39",
+  name: "ROZE News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/212.ts"
+},
+{
+  id: "40",
+  name: "CAPITAL News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/95.ts"
+},
+{
+  id: "41",
+  name: "Sun News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/272964.ts"
+},
+{
+  id: "42",
+  name: "TV 2 day News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/68.ts"
+},
+{
+  id: "43",
+  name: "Channel 5 News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/36935.ts"
+},
+{
+  id: "44",
+  name: "SAMAA News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/141372.ts"
+},
+{
+  id: "45",
+  name: "Chaupaal News",
+  category: "PK | NEWS",
+  logo: "",
+  stream: "http://rolextv.one:80/live/SyedSobanHaidar/03215838388/292068.ts"
+},
+  
