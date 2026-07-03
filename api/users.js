a35737966e281fe10697a0296244aba1ea85359d
animaxxpk <animaxxpk@gmail.com>
2026-07-04 00:48:26 +0500

    users.js

--- api/users.js
+++ api/users.js
@@ -4,12 +4,5 @@ export const users = {
     expiry: "2026-07-07",
     status: "Active",
     max_connections: 1
-  },
-
-  ali: {
-    password: "1234",
-    expiry: "2026-12-31",
-    status: "Active",
-    max_connections: 1
   }
 };
